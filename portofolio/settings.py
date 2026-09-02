9c82688c805c5ad3590bcbd41b6990335ae957a0
Nefi <nafisaand23@gmail.com>
2026-09-02 15:30:18 +0700

    Add PWS allowed host

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'django-insecure-1u%y@=1h7+6c$3rn)*zl7ic&i2akn7hq(6j3*qi3i%46s-y*bj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "nafisa-naila-myportofolio.pws.cs.ui.ac.id"]
 
 
 # Application definition
